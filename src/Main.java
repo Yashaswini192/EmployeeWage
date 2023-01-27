bd184f7c5416ec118a002de0e939d4629ac9a2de
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-01-27 09:33:18 +0530

    added attendance feature

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args){
+
+        int isPresent=0;
+        int isAbsent=1;
+        int checkAttendance=(int)((Math.random()*10)%2);
+        if(checkAttendance==isPresent){
+            System.out.println("Employee is present");
+        }else{
+            System.out.println("Employee is absent");
+        }
+    }
 }
