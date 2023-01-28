a11af459f606fe7aef2fa8965d3d1a5aa12e4536
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-01-28 09:16:36 +0530

    calculating part time

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,20 @@ public class Main {
         int dailySalary;
         int workHours;
         int salaryPerHour = 20;
+        int partTime=8;
         int checkAttendance = (int) ((Math.random() * 10) % 2);
         if (checkAttendance == isPresent) {
             System.out.println("Employee is present");
             workHours = 8;
-        } else {
+        }
+        else if (checkAttendance == isAbsent){
             System.out.println("Employee is absent");
         workHours = 0;
-    }
+        }
+        else {
+            System.out.println("Employee is working part time");
+            workHours=4;
+        }
         dailySalary=workHours*salaryPerHour;
         System.out.println("DailySalary is:"+dailySalary);
     }
