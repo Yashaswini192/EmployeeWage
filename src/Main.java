04fb06823371d2fd0ec7aec401074d90c229073d
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-01-30 18:28:56 +0530

    Solving using switch case

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,28 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         int isPresent = 0;
         int isAbsent = 1;
-        int dailySalary;
-        int workHours;
         int salaryPerHour = 20;
-        int partTime=8;
+        int workHours=16;
         int checkAttendance = (int) ((Math.random() * 10) % 2);
-        if (checkAttendance == isPresent) {
-            System.out.println("Employee is present");
-            workHours = 8;
+        switch (checkAttendance) {
+            case 1 -> {
+                workHours = 16;
+                System.out.println("employee is present");
+            }
+            case 2 -> {
+                workHours = 0;
+                System.out.println("employee is absent");
+            }
+            case 3 -> {
+                workHours = 8;
+                System.out.println("employee is working part time");
+            }
+            default -> System.out.println("  ");
         }
-        else if (checkAttendance == isAbsent){
-            System.out.println("Employee is absent");
-        workHours = 0;
-        }
-        else {
-            System.out.println("Employee is working part time");
-            workHours=4;
-        }
-        dailySalary=workHours*salaryPerHour;
+        int dailySalary = workHours * salaryPerHour;
         System.out.println("DailySalary is:"+dailySalary);
     }
+
 }
